69c30ac18f4887f8265cfa56699da36c25431422
shenshi <shenshi@shouqianba.com>
2022-01-10 19:57:36 +0800

    feat: add images

--- docs/.vuepress/.temp/pages/doc/milestone/index.html.js
+++ docs/.vuepress/.temp/pages/doc/milestone/index.html.js
@@ -5,8 +5,75 @@ export const data = {
   "lang": "en-US",
   "frontmatter": {},
   "excerpt": "",
-  "headers": [],
-  "filePathRelative": "doc/milestone/README.md"
+  "headers": [
+    {
+      "level": 2,
+      "title": "2022.1.1",
+      "slug": "_2022-1-1",
+      "children": [
+        {
+          "level": 3,
+          "title": "不知不觉，到了新的一年啦，开启新的生活，祝愿平安幸福，为梦想继续添把火，加把力！",
+          "slug": "不知不觉-到了新的一年啦-开启新的生活-祝愿平安幸福-为梦想继续添把火-加把力",
+          "children": []
+        }
+      ]
+    },
+    {
+      "level": 2,
+      "title": "2022.1.2",
+      "slug": "_2022-1-2",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "2022.1.3",
+      "slug": "_2022-1-3",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "2022.1.4",
+      "slug": "_2022-1-4",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "2022.1.5",
+      "slug": "_2022-1-5",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "2022.1.6",
+      "slug": "_2022-1-6",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "2022.1.7",
+      "slug": "_2022-1-7",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "2022.1.8",
+      "slug": "_2022-1-8",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "2022.1.9",
+      "slug": "_2022-1-9",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "2022.1.10",
+      "slug": "_2022-1-10",
+      "children": []
+    }
+  ]
 }
 
 if (import.meta.webpackHot) {
--- docs/.vuepress/.temp/pages/doc/milestone/index.html.vue
+++ docs/.vuepress/.temp/pages/doc/milestone/index.html.vue
@@ -1,2 +1,87 @@
 <template><h1 id="里程碑" tabindex="-1"><a class="header-anchor" href="#里程碑" aria-hidden="true">#</a> 里程碑</h1>
+<h2 id="_2022-1-1" tabindex="-1"><a class="header-anchor" href="#_2022-1-1" aria-hidden="true">#</a> 2022.1.1</h2>
+<h3 id="不知不觉-到了新的一年啦-开启新的生活-祝愿平安幸福-为梦想继续添把火-加把力" tabindex="-1"><a class="header-anchor" href="#不知不觉-到了新的一年啦-开启新的生活-祝愿平安幸福-为梦想继续添把火-加把力" aria-hidden="true">#</a> 不知不觉，到了新的一年啦，开启新的生活，祝愿平安幸福，为梦想继续添把火，加把力！</h3>
+<h2 id="_2022-1-2" tabindex="-1"><a class="header-anchor" href="#_2022-1-2" aria-hidden="true">#</a> 2022.1.2</h2>
+<img src="@source/../public/images/git.png" width="100%" height="200">
+<ol>
+<li>
+<p>git：</p>
+<ul>
+<li>
+<p>Git和SVN有什么区别？
+<br/> git是分布式控制管理系统，属于第三代，客户端可以在本地系统上克隆整个仓库，即使离线也可提交，可以用commit共享</p>
+</li>
+<li>
+<p>什么是Git？</p>
+</li>
+<li>
+<p>在 Git 中提交的命令是什么？</p>
+</li>
+<li>
+<p>什么是 Git 中的“裸存储库”？</p>
+</li>
+<li>
+<p>Git 是用什么语言编写的？</p>
+</li>
+</ul>
+</li>
+<li>
+<p>git使用：</p>
+<ul>
+<li>
+<p>在Git中，你如何还原已经 push 并公开的提交？</p>
+</li>
+<li>
+<p>git pull 和 git fetch 有什么区别？</p>
+</li>
+<li>
+<p>git中的“staging area”或“index”是什么？</p>
+</li>
+<li>
+<p>什么是 git stash?</p>
+</li>
+<li>
+<p>什么是git stash drop？</p>
+</li>
+<li>
+<p>如何找到特定提交中已更改的文件列表？</p>
+</li>
+<li>
+<p>git config 的功能是什么？</p>
+</li>
+<li>
+<p>提交对象包含什么？</p>
+</li>
+<li>
+<p>如何在Git中创建存储库？</p>
+</li>
+<li>
+<p>怎样将 N 次提交压缩成一次提交？</p>
+</li>
+<li>
+<p>什么是 Git bisect？如何使用它来确定（回归）错误的来源？</p>
+</li>
+<li>
+<p>如果想要在提交之前运行代码性检查工具，并在测试失败时阻止提交，该怎样配置 Git 存储库？</p>
+</li>
+<li>
+<p>描述一下你所使用的分支策略？</p>
+</li>
+<li>
+<p>如果分支是否已合并为master，你可以通过什么手段知道？</p>
+</li>
+<li>
+<p>什么是SubGit？</p>
+</li>
+</ul>
+</li>
+</ol>
+<h2 id="_2022-1-3" tabindex="-1"><a class="header-anchor" href="#_2022-1-3" aria-hidden="true">#</a> 2022.1.3</h2>
+<h2 id="_2022-1-4" tabindex="-1"><a class="header-anchor" href="#_2022-1-4" aria-hidden="true">#</a> 2022.1.4</h2>
+<h2 id="_2022-1-5" tabindex="-1"><a class="header-anchor" href="#_2022-1-5" aria-hidden="true">#</a> 2022.1.5</h2>
+<h2 id="_2022-1-6" tabindex="-1"><a class="header-anchor" href="#_2022-1-6" aria-hidden="true">#</a> 2022.1.6</h2>
+<h2 id="_2022-1-7" tabindex="-1"><a class="header-anchor" href="#_2022-1-7" aria-hidden="true">#</a> 2022.1.7</h2>
+<h2 id="_2022-1-8" tabindex="-1"><a class="header-anchor" href="#_2022-1-8" aria-hidden="true">#</a> 2022.1.8</h2>
+<h2 id="_2022-1-9" tabindex="-1"><a class="header-anchor" href="#_2022-1-9" aria-hidden="true">#</a> 2022.1.9</h2>
+<h2 id="_2022-1-10" tabindex="-1"><a class="header-anchor" href="#_2022-1-10" aria-hidden="true">#</a> 2022.1.10</h2>
 </template>
